0ac360779172109636dda0c4121c20fbd1b44f87
Metehan <metehan.bolat.ie@gmail.com>
2022-10-12 16:59:13 +0300

    First card was completed.

--- app/src/main/java/com/metehanbolat/uistructurexml/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/uistructurexml/MainActivity.kt
@@ -3,14 +3,15 @@ package com.metehanbolat.uistructurexml
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.metehanbolat.uistructurexml.databinding.ActivityMainBinding
+import com.metehanbolat.uistructurexml.databinding.CalorieCounterUiBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: CalorieCounterUiBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = CalorieCounterUiBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
